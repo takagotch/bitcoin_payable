6874f9fdc8746a00d94799350ce1c23577dc7568
takagotch <30462722+takagotch@users.noreply.github.com>
2019-07-15 18:11:12 +0900

    Create btc_payment_transaction.rb

---
+++ fr/lib/bitcoin_payable/btc_payment_transaction.rb
@@ -0,0 +1,18 @@
+module BitcoinPayable
+  class BitcoinPaymentTransaction < ::ActiveRecord::Base
+    
+    belongs_to :bitcoin_payment
+    
+    before_save :add_conversion
+    
+    def add_conversion
+      adapter = BitcoinPayable::Adapters::Base.fetch_adapter
+      self.btc_conversion ||= bitcoin_payment.btc_conversion
+    end
+    
+    def confirmed?
+      self.confirmations.to_i >= BitcoinPayable.config.confirmations
+    end
+  end
+end
+
