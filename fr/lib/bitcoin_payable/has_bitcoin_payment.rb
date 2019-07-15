b3ee2b7997d9590f9127c5e3966f375dbf509386
takagotch <30462722+takagotch@users.noreply.github.com>
2019-07-15 18:26:50 +0900

    Create has_bitcoin_payment.rb

---
+++ fr/lib/bitcoin_payable/has_bitcoin_payment.rb
@@ -0,0 +1,15 @@
+module BitcoinPayable
+  module Model
+    
+    def self.included(base)
+      base.send :extend, ClassMethods
+    end
+    
+    module ClassMethod
+    
+      def has_bitcoin_payments(options = {})
+        has_many :bitcoin_payments, -> {order(:id)}, class_name: BitcoinPayable::BitcoinPayment, as: 'payable'
+      end
+      
+  end
+end
